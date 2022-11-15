9b7448fa544a87fe52d523623006a696adb0cea4
phandai032 <phandai032@gmail.com>
2022-11-15 10:58:51 +0700

    healthcare quoc te

---
+++ healthcare/healthcare/doctype/therapy_session/test_therapy_session.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2020, Frappe Technologies Pvt. Ltd. and Contributors
+# See license.txt
+
+
+from frappe.tests.utils import FrappeTestCase
+
+import frappe
+from frappe.utils import nowdate
+
+
+class TestTherapySession(FrappeTestCase):
+	pass
+
+
+def create_therapy_session(patient, therapy_type, therapy_plan, duration=0, start_date=None):
+	if not start_date:
+		start_date = nowdate()
+	therapy_session = frappe.new_doc("Therapy Session")
+	therapy_session.patient = patient
+	therapy_session.therapy_type = therapy_type
+	therapy_session.therapy_plan = therapy_plan
+	therapy_session.duration = duration
+	therapy_session.start_date = start_date
+	therapy_session.save()
+
+	return therapy_session
